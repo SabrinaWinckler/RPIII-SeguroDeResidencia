38e90136fadc2318dd8c95f9f60c7b81245a440d
Matheus Montanha <matheusmontanhakk@gmail.com>
2018-04-25 10:09:39 -0300

    Criação dos getters e setters
    Criação dos getters e setters dos atributos de Residencia e a adição de um List de bens.

--- Codigo/src/dadosResidencia/Residencia.java
+++ Codigo/src/dadosResidencia/Residencia.java
@@ -1,42 +1,36 @@
 package dadosResidencia;
 
-
-import dadosResidencia.Local;
-import dadosResidencia.NivelDePericulosidade;
-import dadosResidencia.Bem;
-import dadosResidencia.Bem;
-import dadosResidencia.Local;
-import dadosResidencia.NivelDePericulosidade;
+import java.util.List;
 
 public class Residencia {
 
-	private String descricaoRes;
+    private String descricaoRes;
 
-	private int numRes;
+    private int numRes;
 
-	private long cepRes;
+    private long cepRes;
 
-	private int qntComodos;
+    private int qntComodos;
 
-	private int qntBanheiros;
+    private int qntBanheiros;
 
-	private int qntGaragens;
+    private int qntGaragens;
 
-	private float areaTotal;
+    private float areaTotal;
 
-	private int numAndares;
+    private int numAndares;
 
-	private int anoConstrucao;
+    private int anoConstrucao;
 
-	private String ruaRes;
+    private String ruaRes;
 
-	private NivelDePericulosidade nivelDePericulosidade;
+    private NivelDePericulosidade nivelDePericulosidade;
 
-	private Bem bem;
-        
-	private Local local;
+    private List<Bem> bem;
 
-    public Residencia(String descricaoRes, int numRes, long cepRes, int qntComodos, int qntBanheiros, int qntGaragens, float areaTotal, int numAndares, int anoConstrucao, String ruaRes,NivelDePericulosidade nivelDePericulosidade, Local local) {
+    private Local local;
+
+    public Residencia(String descricaoRes, int numRes, long cepRes, int qntComodos, int qntBanheiros, int qntGaragens, float areaTotal, int numAndares, int anoConstrucao, String ruaRes, NivelDePericulosidade nivelDePericulosidade, Local local) {
         this.descricaoRes = descricaoRes;
         this.numRes = numRes;
         this.cepRes = cepRes;
@@ -51,7 +45,18 @@ public class Residencia {
         this.local = local;
         this.bem = null;
     }
-    public void setBem(Bem bem) {
+
+    /**
+     * @return the bem
+     */
+    public List<Bem> getBem() {
+        return bem;
+    }
+
+    /**
+     * @param bem the bem to set
+     */
+    public void setBem(List<Bem> bem) {
         this.bem = bem;
     }
 
@@ -59,33 +64,182 @@ public class Residencia {
         this.local = local;
     }
 
-    public Bem getBem() {
-        return bem;
-    }
-
     public Local getLocal() {
         return local;
     }
 
+    /**
+     * @return the descricaoRes
+     */
+    public String getDescricaoRes() {
+        return descricaoRes;
+    }
 
-	public void registraResidencia() {
+    /**
+     * @param descricaoRes the descricaoRes to set
+     */
+    public void setDescricaoRes(String descricaoRes) {
+        this.descricaoRes = descricaoRes;
+    }
 
-	}
+    /**
+     * @return the numRes
+     */
+    public int getNumRes() {
+        return numRes;
+    }
 
-	public void registraLocalizacao(String endereco, long cep) {
+    /**
+     * @param numRes the numRes to set
+     */
+    public void setNumRes(int numRes) {
+        this.numRes = numRes;
+    }
 
-	}
+    /**
+     * @return the cepRes
+     */
+    public long getCepRes() {
+        return cepRes;
+    }
 
-	public void registraInfo(String descricao, int anoConstrucao) {
+    /**
+     * @param cepRes the cepRes to set
+     */
+    public void setCepRes(long cepRes) {
+        this.cepRes = cepRes;
+    }
 
-	}
+    /**
+     * @return the qntComodos
+     */
+    public int getQntComodos() {
+        return qntComodos;
+    }
 
-	public void registraTamanho(float areaT, int numAndares, int qntGaragem, int qntComodo, int qntBanheiro) {
+    /**
+     * @param qntComodos the qntComodos to set
+     */
+    public void setQntComodos(int qntComodos) {
+        this.qntComodos = qntComodos;
+    }
 
-	}
+    /**
+     * @return the qntBanheiros
+     */
+    public int getQntBanheiros() {
+        return qntBanheiros;
+    }
 
-	public void calculaNivelPericulosidade() {
+    /**
+     * @param qntBanheiros the qntBanheiros to set
+     */
+    public void setQntBanheiros(int qntBanheiros) {
+        this.qntBanheiros = qntBanheiros;
+    }
 
-	}
+    /**
+     * @return the qntGaragens
+     */
+    public int getQntGaragens() {
+        return qntGaragens;
+    }
+
+    /**
+     * @param qntGaragens the qntGaragens to set
+     */
+    public void setQntGaragens(int qntGaragens) {
+        this.qntGaragens = qntGaragens;
+    }
+
+    /**
+     * @return the areaTotal
+     */
+    public float getAreaTotal() {
+        return areaTotal;
+    }
+
+    /**
+     * @param areaTotal the areaTotal to set
+     */
+    public void setAreaTotal(float areaTotal) {
+        this.areaTotal = areaTotal;
+    }
+
+    /**
+     * @return the numAndares
+     */
+    public int getNumAndares() {
+        return numAndares;
+    }
+
+    /**
+     * @param numAndares the numAndares to set
+     */
+    public void setNumAndares(int numAndares) {
+        this.numAndares = numAndares;
+    }
+
+    /**
+     * @return the anoConstrucao
+     */
+    public int getAnoConstrucao() {
+        return anoConstrucao;
+    }
+
+    /**
+     * @param anoConstrucao the anoConstrucao to set
+     */
+    public void setAnoConstrucao(int anoConstrucao) {
+        this.anoConstrucao = anoConstrucao;
+    }
+
+    /**
+     * @return the ruaRes
+     */
+    public String getRuaRes() {
+        return ruaRes;
+    }
+
+    /**
+     * @param ruaRes the ruaRes to set
+     */
+    public void setRuaRes(String ruaRes) {
+        this.ruaRes = ruaRes;
+    }
+
+    /**
+     * @return the nivelDePericulosidade
+     */
+    public NivelDePericulosidade getNivelDePericulosidade() {
+        return nivelDePericulosidade;
+    }
+
+    /**
+     * @param nivelDePericulosidade the nivelDePericulosidade to set
+     */
+    public void setNivelDePericulosidade(NivelDePericulosidade nivelDePericulosidade) {
+        this.nivelDePericulosidade = nivelDePericulosidade;
+    }
+
+    public void registraResidencia() {
+
+    }
+
+    public void registraLocalizacao(String endereco, long cep) {
+
+    }
+
+    public void registraInfo(String descricao, int anoConstrucao) {
+
+    }
+
+    public void registraTamanho(float areaT, int numAndares, int qntGaragem, int qntComodo, int qntBanheiro) {
+
+    }
+
+    public void calculaNivelPericulosidade() {
+
+    }
 
 }
