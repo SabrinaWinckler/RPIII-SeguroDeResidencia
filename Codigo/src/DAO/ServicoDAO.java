fd273c9a5fb2576220c45c22029f5e8804c60952
Débora Siqueira <deborasiqueira_99@hotmail.com>
2018-06-22 14:43:19 -0300

    Implementações no Solicitar Serviço e exclusão de telas desnecessárias
    Realizei algumas implementações no solicitar serviço, colocando a opção do usuário escolher a data desejada etc. Agora tudo esta sendo realizado no painel do candidato, antes estava em telas separadas.

--- Codigo/src/DAO/ServicoDAO.java
+++ Codigo/src/DAO/ServicoDAO.java
@@ -7,10 +7,16 @@ package DAO;
 
 import Dominio.Servico;
 import Dominio.Bem;
+import Dominio.ItemServico;
+import Dominio.Segurado;
+import com.sun.javafx.geom.Quat4f;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -25,10 +31,14 @@ public class ServicoDAO {
     public void create(Servico servico) {
         Connection conexao = ConnectionFactory.realizarConexao();
         PreparedStatement stm = null;
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            stm = conexao.prepareStatement("INSERT INTO servico(descricaoServico, quantidadeServico)VALUES(?,?)");
+            stm = conexao.prepareStatement("INSERT INTO servico(descricaoServico, "
+                    + "quantidadeServico, dataDeSolicitacao, dataVisita)VALUES(?,?,?,?)");
             stm.setString(1, servico.getDesc());
             stm.setFloat(2, servico.getQnt());
+            stm.setDate(3, java.sql.Date.valueOf(sdf.format(servico.getDataDeSolitacao())));
+            stm.setDate(4, java.sql.Date.valueOf(sdf.format(servico.getDataDeVisita())));
             stm.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(BemDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -59,4 +69,39 @@ public class ServicoDAO {
         return servico;
     }
 
+    public List<ItemServico> servicosSegurados(Segurado segurado) {
+        Connection conexao = ConnectionFactory.realizarConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<ItemServico> listaDeServico = new ArrayList<>();
+        try {
+            stmt = conexao.prepareStatement("select * from servico inner join itemservico on "
+                    + "itemservico.idServico = servico.idServico inner join seguradosolicitaservico on "
+                    + "seguradosolicitaservico.idServico = itemservico.idServico and "
+                    + "seguradosolicitaservico.idSegurado =" + segurado.getCodPessoa());
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                ItemServico itemServico = new ItemServico();
+                itemServico.setDesc(rs.getString("descricaoServico"));
+                itemServico.setIdCodServico(rs.getInt("idServico"));
+                itemServico.setQnt(rs.getInt("quantidadeServico"));
+                itemServico.setDescricaoRecusa(rs.getString("descricaoRecusa"));
+                itemServico.setDescricaoAtendimento(rs.getString("descricaoAtendimento"));
+                itemServico.setDataDeAtendimento(rs.getDate("dataAtendimento"));
+                itemServico.setAtendidaSolicitacaoServico(rs.getString("atendidaSolicitacao"));
+                itemServico.setAceitaSolicitacao(rs.getString("aceitaSolicitacao"));
+                itemServico.setDescricaoSolicitacao(rs.getString("descricaoSolicitacao"));
+                itemServico.setDataSolicitacaoServico(rs.getDate("dataSolicitacao"));
+                itemServico.setIdItemServico(rs.getInt("idServico"));
+                listaDeServico.add(itemServico);
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(Bem.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            ConnectionFactory.fecharConexao(conexao, stmt, rs);
+        }
+
+        return listaDeServico;
+
+    }
 }
--- Codigo/src/Dominio/Servico.java
+++ Codigo/src/Dominio/Servico.java
@@ -5,20 +5,36 @@
  */
 package Dominio;
 
+import java.util.Date;
+
 /**
  *
  * @author Matheus Montanha
  */
 public class Servico {
+
+    private int idCodServico;
     private String desc;
     private int qnt;
+    private Date dataDeSolitacao;
+    private Date dataDeVisita;
 
     public Servico() {
     }
 
-    public Servico(String desc, int qnt) {
+    public Servico(int codServico, String desc, int qnt, Date data, Date visitaResidencia) {
         this.desc = desc;
         this.qnt = qnt;
+        this.idCodServico = codServico;
+        this.dataDeSolitacao = data;
+        this.dataDeVisita = visitaResidencia;
+    }
+
+    public Servico(String desc, int qnt, Date data, Date visitaResidencia) {
+        this.desc = desc;
+        this.qnt = qnt;
+        this.dataDeSolitacao = data;
+        this.dataDeVisita = visitaResidencia;
     }
 
     public String getDesc() {
@@ -36,6 +52,47 @@ public class Servico {
     public void setQnt(int qnt) {
         this.qnt = qnt;
     }
-    
-    
+
+    /**
+     * @return the idCodServico
+     */
+    public int getIdCodServico() {
+        return idCodServico;
+    }
+
+    /**
+     * @param idCodServico the idCodServico to set
+     */
+    public void setIdCodServico(int idCodServico) {
+        this.idCodServico = idCodServico;
+    }
+
+    /**
+     * @return the dataDeSolitacao
+     */
+    public Date getDataDeSolitacao() {
+        return dataDeSolitacao;
+    }
+
+    /**
+     * @param dataDeSolitacao the dataDeSolitacao to set
+     */
+    public void setDataDeSolitacao(Date dataDeSolitacao) {
+        this.dataDeSolitacao = dataDeSolitacao;
+    }
+
+    /**
+     * @return the dataDeVisita
+     */
+    public Date getDataDeVisita() {
+        return dataDeVisita;
+    }
+
+    /**
+     * @param dataDeVisita the dataDeVisita to set
+     */
+    public void setDataDeVisita(Date dataDeVisita) {
+        this.dataDeVisita = dataDeVisita;
+    }
+
 }
